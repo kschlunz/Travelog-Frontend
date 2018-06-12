799cadbe928e92c7065086cff4522c60a3c6cbb1
github username <github email address>
2018-06-12 18:15:21 -0400

    Links to pages working

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
 const TripList = (props) => {
 
   console.log(props.tripData)
 
     return (
-    <li ><h1>Trip: {props.tripData.name}</h1> <p>Dates: {props.tripData.dates}</p><p>Description:{props.tripData.description}</p></li>
+    <li ><h1><Link to={`/trips/${props.tripData.tripID}`}>Trip: {props.tripData.name}</Link></h1> <p>Dates: {props.tripData.dates}</p><p>Description:{props.tripData.description}</p></li>
 
   )
 }
