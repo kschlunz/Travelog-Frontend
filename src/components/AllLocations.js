9815a6ba183afea34e2abd064f6f55d886a94b84
github username <github email address>
2018-06-12 21:06:51 -0400

    new bugs, can't add new places- post request problem

--- src/components/AllLocations.js
+++ src/components/AllLocations.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const AllLocations = (props) => {
 
-  console.log(props.tripData)
+  console.log(props.tripData.location)
 
     return (
     <li ><Link to={`/places/${props.tripData.locationID}`}><h2>Place: {props.tripData.location}</h2> </Link></li>
